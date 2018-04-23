b44d9c9486c3a2295e0dd11fe33cbb0bfbed931d
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-04-23 11:32:30 +0200

    c1

---
+++ GitUsage/GitUsage/Cheer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitUsage
+{
+    static class Cheer
+    {
+        public static void Cheers()
+        {
+            Console.WriteLine("This is hello from Member C");
+        }
+    }
+}
