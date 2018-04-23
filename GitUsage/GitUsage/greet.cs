c2144982a8871dd20bc00679865011b0f6305edf
DeveloperADJ <developeradj@gmail.com>
2018-04-23 12:03:11 +0200

    cleaned up greeter method

--- GitUsage/GitUsage/greet.cs
+++ GitUsage/GitUsage/greet.cs
@@ -6,13 +6,6 @@ public class Greeter
 
     void Greet(string name, int age)
     {
-<<<<<<< HEAD
-        Console.WriteLine("Greetings");
-        Greeting = "Sup homies!";
-
-        Console.WriteLine(Greeting);
-=======
         Console.WriteLine($"Hello {name} you are {age} years old.");
->>>>>>> Arno-twoparam
     }
 }
